6bba25d7bc8d4fa594851e12c758df9f018ba454
carlo <carlo@DESKTOP-U6U7T0H>
2024-10-03 14:26:10 +0200

    - 03/10/2024

---
+++ src/funciones/TestFunciones.py
@@ -0,0 +1,14 @@
+'''
+Created on 3 oct 2024
+
+@author: carlo
+'''
+
+#Funcion 1
+def funcion1(n:int,k:int):
+    if n>k:
+        for n in range(2,n+1):
+            rere = 0   
+        return rere
+    else:
+        print('El primer numero que das tiene que ser mayor que el segundo')
