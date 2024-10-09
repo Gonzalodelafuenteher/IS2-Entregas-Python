b121dc0ef4465899e3a23e211cfec590f0c0db6b
gonza <gonza@PCGonzalo>
2024-10-09 18:52:05 +0200

    - 09/10/2024

---
+++ src/Lecturas/lecturas.py
@@ -0,0 +1,7 @@
+'''
+Created on 9 oct 2024
+
+@author: gonza
+'''
+
+if __name__ == '__main__':
--- src/funciones/TestFunciones.py
+++ src/funciones/TestFunciones.py
@@ -3,7 +3,7 @@ Created on 4 oct 2024
 
 @author: carlo
 '''
-
+import math
 
 #Funcion 1
 def funcion1(n:int,k:int):
@@ -12,18 +12,49 @@ def funcion1(n:int,k:int):
             s = s * (n - i + 1)
         return s
 
+#funcion 2
+
+def funcion2(a1:int,r:int,k:int):
+    var_producto:int = 1
+    for n in range(k+1):
+        an = a1 * ( r ** ( n-1 ) )
+        var_producto = var_producto * an
+    return var_producto
+
+
+
+
+#funcion3 
+def funcion3(n,k):  
+    if n>=k:
+        return  math.factorial(n)/((math.factorial(k))*(math.factorial(n-k)))
+
+
+#funcion4
+
 
 
 if __name__ == '__main__':
-    n=3
-    k= 4
-    if n>k:
+        n=5
+        k= 4
+        if n>k:
+            
+            print(f"El resultado del producto es: {funcion1(n,k)}")
+            
+        else:
+            print('El numero n tienes que ser mayor que k ')
+        ################
+        a1 = 2
+        r = 2
+        k2= 4
         
-        print(f"El resultado del producto es: {funcion1(n,k)}")
+        print( funcion2 (a1,r,k2))
         
-    else:
-        print('El numero n tienes que ser mayor que k ')
+        #################
+        n1 = 5
+        k3 = 2
+        print (funcion3(n1,k3))
         
+        #################
         
-   
     
