d02bd2af5e32f84a03152c2145ece6829f811bb3
gonza <gonza@PCGonzalo>
2024-10-12 20:41:37 +0200

    -12/10/2024

--- src/Lecturas/Testlecturas.py
+++ src/Lecturas/Testlecturas.py
@@ -1,10 +1,28 @@
 '''
-Created on 9 oct 2024
+Created on 12 oct 2024
 
 @author: gonza
 '''
+def Funcion1_6(file:str,separador:str,palabra:str) -> int:
+    with open(file,encoding='UTF-8') as f:
+        r:list[str] = []
+        contador:int = 0
+        for linea in f:
+            for p in linea.split(separador):
+                p = p.strip()
+                if len(p) > 0:
+                    r.append(p.lower())
+    print (r)
+    for i in r:
+        if i  == palabra:
+            contador = contador + 1
+      
+    return contador
 
 
-
-
-
+if __name__ == '__main__':
+    
+    palabra = "quijote"
+    separador = " "
+    resultado = Funcion1_6("lin_quijote.txt" , separador , palabra)
+    print(f'La palabra {palabra} se repite {resultado}')
--- src/funciones/fsgdas.py
+++
@@ -1,6 +0,0 @@
-'''
-Created on 10 oct 2024
-
-@author: carlo
-'''
-
