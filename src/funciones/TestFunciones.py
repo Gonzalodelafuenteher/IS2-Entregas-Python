8474e1dc6073e79734b3916853baf7a7b7cdba94
gonza <gonza@PCGonzalo>
2024-10-19 17:13:42 +0200

    - solo falta funcion 5

--- src/funciones/TestFunciones.py
+++ src/funciones/TestFunciones.py
@@ -16,7 +16,7 @@ def funcion1(n:int,k:int):
 
 def funcion2(a1:int,r:int,k:int):
     var_producto:int = 1
-    for n in range(k+1):
+    for n in range(1,k+1):
         an = a1 * ( r ** ( n-1 ) )
         var_producto = var_producto * an
     return var_producto
@@ -44,11 +44,14 @@ def funcion4(n,k):
     else:
         print("n tiene que ser mayor que k")  
 #funcion5
-def funcion5(a,epsi,f,fd):
-    if f(a) >= epsi:
-        resultado = a - (f(a)/fd(a))
+def funcion5(e,f, f_prime, a):
+    
+    f0 = f(a)
+    fd0 = fd(a)
+    if f(a) >= e:
+        resultado = a - (abs(f0)/abs(fd0))
     return resultado
-        
+       
     
     
 
@@ -60,40 +63,40 @@ if __name__ == '__main__':
         k:int = 2
         if n>k:
             
-            print(f"El resultado del producto es: {funcion1(n,k)}")
+            print(f"El producto de {n} y {k} es: {funcion1(n,k)}")
             
         else:
             print('El numero n tienes que ser mayor que k ')
 
         print ('################################################')
         
-        a1:int = 2
-        r:int = 2
+        a1:int = 3
+        r:int = 5
         k2:int= 2
         
-        print( funcion2 (a1,r,k2))
+        print(f"El producto de la secuencia geométrica con a1 = {a1}, r = {r} y k = {k2} es: {funcion2 (a1,r,k2)}")
         
         print ('################################################')
         
         
         n1:int = 4
         k3:int = 2
-        print (funcion3(n1,k3))
+        print (f"El producto de {n1} y {k3} es: {funcion3(n1,k3)}")
         
         print ('################################################')
         
         
         n2 = 4
-        k4 = 1
+        k4 = 2
         
-        print(funcion4(n2,k4))
+        print(f"El número S(n,k) siendo n = {n2} y k = {k4} es: {funcion4(n2,k4)}")
         
         print ('################################################')
         a:float = 3
         epsilon:float= 0.001
         f = lambda x: 2*x**2
         fd = lambda x: 4*x
-        print(funcion5(a, epsilon, f, fd))
+        print(f"Resultado de la función 5 con a = {a} y e = {epsilon}, f(x) = {f} y f'(x) = {fd}  : {funcion5(epsilon, f, fd,a)}")
     
        
         
