164480b39eb3d2c1062aeb573db575ef18b162d4
gonza <gonza@PCGonzalo>
2024-10-20 18:07:24 +0200

    -TERMINADO

--- src/Lecturas/Testlecturas.py
+++ src/Lecturas/Testlecturas.py
@@ -48,6 +48,8 @@ def longitud_promedio_lineas(file_path: str) -> float:
         for linea in f:
             longitud_lineas = longitud_lineas +len(linea.split(","))
             longitud_total = longitud_total + 1
+        if longitud_total == 0:
+            return print("none")
     return longitud_lineas/longitud_total  
     
     
@@ -56,7 +58,7 @@ def longitud_promedio_lineas(file_path: str) -> float:
         
 if __name__ == '__main__': 
     print ('################################################')
-   
+    print("TEST DE LA FUNCION 6: ")
     palabra = "quijote"
     file1 =  '../../'  + "Resources/lin_quijote.txt"
     separador = " "
@@ -65,6 +67,7 @@ if __name__ == '__main__':
     
     
     print ('################################################')
+    print("TEST DE LA FUNCION 7: ")
     palabra2 = "quijote"
     file2 = '../../'  + "Resources/lin_quijote.txt"
     resultado2 = funcion2_7(file2 , palabra2)
@@ -72,18 +75,20 @@ if __name__ == '__main__':
     
     
     print ('################################################')
+    print("TEST DE LA FUNCION 8: ")
 
     file3 = '../../'  + "Resources/archivo_palabras.txt"
     resultado3 = funcion3_8(file3)
     print (f'Las palabras únicas en el fichero {file3} son: {resultado3} \n')
 
     print ('################################################')
-    
+    print("TEST DE LA FUNCION 9: ")
     file4 = '../../'  + "Resources/palabras_random.csv"
     file5 = '../../'  + "Resources/vacio.csv"
     resultado4 = longitud_promedio_lineas(file4)
-    
     resultado5 = longitud_promedio_lineas(file5)
-    print(f'La longitud promedio de las líneas del fichero {file4} es: {resultado4}')
+    print(f'La longitud promedio de las líneas del fichero {file4} es: {resultado4} \n')
+    
+    print("TEST DE LA FUNCION 9: ")
     print(f'La longitud promedio de las líneas del fichero {file5} es: {resultado5}')
 
--- src/funciones/TestFunciones.py
+++ src/funciones/TestFunciones.py
@@ -44,13 +44,13 @@ def funcion4(n,k):
     else:
         print("n tiene que ser mayor que k")  
 #funcion5
-def funcion5(e,f, f_prime, a):
-    
-    f0 = f(a)
-    fd0 = fd(a)
-    if f(a) >= e:
-        resultado = a - (abs(f0)/abs(fd0))
-    return resultado
+def funcion5(e,f, f_prima, a):
+    while abs(f(a)) >= e:
+        f0 = f(a)
+        fd0 = f_prima(a)
+        
+        a = a - f0 / fd0
+    return a
        
     
     
@@ -59,44 +59,47 @@ def funcion5(e,f, f_prime, a):
 if __name__ == '__main__':
     
         print ('################################################')
+        print("TEST DE LA FUNCION 1: ")
         n:int = 4
         k:int = 2
         if n>k:
             
-            print(f"El producto de {n} y {k} es: {funcion1(n,k)}")
+            print(f"El producto de {n} y {k} es: {funcion1(n,k)} \n")
             
         else:
             print('El numero n tienes que ser mayor que k ')
 
         print ('################################################')
+        print("TEST DE LA FUNCION 2: ")
         
         a1:int = 3
         r:int = 5
         k2:int= 2
         
-        print(f"El producto de la secuencia geométrica con a1 = {a1}, r = {r} y k = {k2} es: {funcion2 (a1,r,k2)}")
+        print(f"El producto de la secuencia geométrica con a1 = {a1}, r = {r} y k = {k2} es: {funcion2 (a1,r,k2)} \n")
         
         print ('################################################')
-        
-        
+        print("TEST DE LA FUNCION 3: ")
+    
         n1:int = 4
         k3:int = 2
-        print (f"El producto de {n1} y {k3} es: {funcion3(n1,k3)}")
+        print (f"El producto de {n1} y {k3} es: {funcion3(n1,k3)} \n")
         
         print ('################################################')
-        
+        print("TEST DE LA FUNCION 4: ")
         
         n2 = 4
         k4 = 2
         
-        print(f"El número S(n,k) siendo n = {n2} y k = {k4} es: {funcion4(n2,k4)}")
+        print(f"El número S(n,k) siendo n = {n2} y k = {k4} es: {funcion4(n2,k4)} \n")
         
         print ('################################################')
+        print("TEST DE LA FUNCION 5: ")
         a:float = 3
         epsilon:float= 0.001
         f = lambda x: 2*x**2
         fd = lambda x: 4*x
-        print(f"Resultado de la función 5 con a = {a} y e = {epsilon}, f(x) = {f} y f'(x) = {fd}  : {funcion5(epsilon, f, fd,a)}")
+        print(f"Resultado de la función 5 con a = {a} y e = {epsilon},  f(x) = 2x^2 y f'(x) = 4x  : {funcion5(epsilon, f, fd,a)} \n")
     
        
         
